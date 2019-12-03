153cd5cdaa3d3a6da48fb903a2e83f457e220c5c
Russell Bentulan <russellbentulan@gmail.com>
2019-12-03 10:34:33 -0500

    Updates

---
+++ precache-manifest.736ede5668d4c8a07ee6714bba95a36e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49c25b8f866a46cce846de0b553d8a7",
+    "url": "/russell-bentulan-project-five/index.html"
+  },
+  {
+    "revision": "35304ee0bc817c92a818",
+    "url": "/russell-bentulan-project-five/static/css/main.a5e538e2.chunk.css"
+  },
+  {
+    "revision": "ac809d30024d957c9038",
+    "url": "/russell-bentulan-project-five/static/js/2.c616eda3.chunk.js"
+  },
+  {
+    "revision": "35304ee0bc817c92a818",
+    "url": "/russell-bentulan-project-five/static/js/main.bfd8176a.chunk.js"
+  },
+  {
+    "revision": "581752fe515dbdbf248e",
+    "url": "/russell-bentulan-project-five/static/js/runtime-main.e4ff51fa.js"
+  },
+  {
+    "revision": "d112114058ff18d508e767e7f8334395",
+    "url": "/russell-bentulan-project-five/static/media/paper_fibers.d1121140.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/russell-bentulan-project-five/precache-manifest.736ede5668d4c8a07ee6714bba95a36e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/russell-bentulan-project-five/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
